b38fd13da394aa3e36e7899dab14e14d40ced7da
SandroWeber02 <sandroweber085@gmail.com>
2026-05-20 22:04:25 -0300

    chore: remove dist from repository

--- dist/modules/alunos/alunos.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const alunos_controller_1 = require("./alunos.controller");
-const router = (0, express_1.Router)();
-router.post("/", alunos_controller_1.createAluno);
-exports.default = router;
--- dist/modules/matriculas/matriculas.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const matriculas_controller_1 = require("./matriculas.controller");
-const router = (0, express_1.Router)();
-router.post("/", matriculas_controller_1.createMatricula);
-exports.default = router;
